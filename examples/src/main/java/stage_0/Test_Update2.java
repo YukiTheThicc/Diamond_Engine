74044bf7f4313661e8b5d001f13eb128f240a35d
Santiago Barreiro Chapela <soporte.owcsgp@altia.es>
2025-10-22 16:11:00 +0200

    Sync update. Example for update 2 with transform matrix

--- examples/src/main/java/stage_0/Test_Update2.java
+++ examples/src/main/java/stage_0/Test_Update2.java
@@ -4,6 +4,9 @@ import api.*;
 import assets.Texture;
 import core.InputController;
 import core.glRenderer.*;
+import org.joml.Math;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import utils.Logger;
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_A;
@@ -29,27 +32,7 @@ public class Test_Update2 {
 
     // ATTRIBUTES
     static DiaLogger logger;
-    static GLShader triangleShader;
     static GLShader textureShader;
-
-    static final String vertexShaderSource =
-            "#version 330 core\n" +
-                    "layout (location = 0) in vec3 aPos;\n" +
-                    "layout (location = 1) in vec3 aCol;\n" +
-                    "out vec3 fCol;\n" +
-                    "void main()\n" +
-                    "{\n" +
-                    "   fCol = aCol;\n" +
-                    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" +
-                    "}\0";
-    static final String fragmentShaderSource =
-            "#version 330 core\n" +
-                    "in vec3 fCol;\n" +
-                    "out vec4 FragColor;\n" +
-                    "void main()\n" +
-                    "{\n" +
-                    "   FragColor = vec4(fCol, 1);\n" +
-                    "}\n\0";
 
     static final String vertexShaderTextureSource =
             "#version 330 core\n" +
@@ -58,9 +41,10 @@ public class Test_Update2 {
                     "layout (location = 2) in vec2 aTexCoords;\n" +
                     "out vec3 fCol;\n" +
                     "out vec2 texCoords;\n" +
+                    "uniform mat4 transform;\n" +
                     "void main()\n" +
                     "{\n" +
-                    "   gl_Position = vec4(aPos, 1.0);\n" +
+                    "   gl_Position = transform * vec4(aPos, 1.0);\n" +
                     "   fCol = aCol;\n" +
                     "   texCoords = vec2(aTexCoords.x, aTexCoords.y);\n" +
                     "}\0";
@@ -77,7 +61,7 @@ public class Test_Update2 {
 
     public static void main(String[] args) {
 
-        DiaLogger logger = new Logger();
+        logger = new Logger();
         DiaWindow window = GLWindow.get();
         InputController inputcontroller = new InputController();
         window.init(inputcontroller);
@@ -85,12 +69,13 @@ public class Test_Update2 {
         DiaAssetLoader assetLoader = new GLAssetLoader(logger);
         DiaRenderer renderer = new GLRenderer(logger);
         renderer.init();
-        int VAO = setupTriangle();
         int texturedVAO = setupTexturedExample();
 
         String exampleTexture = System.getProperty("user.dir") + "\\examples\\res\\top.png";
         System.out.println("Attempting to load example texture from = " + exampleTexture + "...");
         Texture texture = assetLoader.loadTexture(exampleTexture);
+
+        Matrix4f trans;
 
         boolean running = true;
         float dt = 0;
@@ -100,13 +85,12 @@ public class Test_Update2 {
 
             window.pollEvents();
 
-            triangleShader.use();
-            glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-            triangleShader.detach();
-
+            trans = new Matrix4f().identity();
+            trans = trans.rotate(bt, new Vector3f(0f,0f,1f));
+            trans = trans.rotate(bt, new Vector3f(0f,1f,0f));
             glBindTexture(GL_TEXTURE_2D, texture.getId());
             textureShader.use();
+            textureShader.uploadMat4f("transform", trans);
             glBindVertexArray(texturedVAO);
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
             textureShader.detach();
@@ -127,39 +111,15 @@ public class Test_Update2 {
         window.close();
     }
 
-    private static int setupTriangle() {
-        triangleShader = new GLShader(vertexShaderSource, fragmentShaderSource);
-        triangleShader.compile(logger);
-
-        float[] vertices = {
-                -1f,    -0.5f,   0.0f, 1f, 0f, 0f,  // left
-                0f,     -0.5f,   0.0f, 0f, 1f, 0f,  // right
-                -0.5f,   0.5f,   0.0f, 0f, 0f, 1f  // top
-        };
-
-        int VBO, VAO;
-        VAO = glGenVertexArrays();
-        VBO = glGenBuffers();
-        glBindVertexArray(VAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.BYTES, 0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
-        glEnableVertexAttribArray(1);
-        return VAO;
-    }
-
     private static int setupTexturedExample() {
         textureShader = new GLShader(vertexShaderTextureSource, fragmentShaderTextureSource);
         textureShader.compile(logger);
 
         float[] vertices = {
-                1f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-                1f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-                0f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-                0f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+                0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+                0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+                -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+                -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
         };
 
         int[] indices = {
